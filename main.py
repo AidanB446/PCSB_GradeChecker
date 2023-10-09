d38f8826c22668460fa9b5305a04541e51db57c1
AidanB446 <138682950+AidanB446@users.noreply.github.com>
2023-10-09 16:58:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,22 +8,25 @@ from selenium.webdriver.common.by import By
 
 def getGrades(username, password):
 
-
+   # setup webdriver in headless mode.
    options = webdriver.EdgeOptions()
    options.add_argument('--headless')
    driver = webdriver.Edge(options=options)
-   
+
+   # navigate to the sign in page
    driver.get("https://focus.pcsb.org/focus/Modules.php?modname=misc/Portal.php")
    print('logging in')
-   
+
+   # login with credentials user provided
    driver.find_element(by=By.ID, value="username-input").send_keys(username)
    driver.find_element(by=By.NAME, value="password").send_keys(password)
    driver.find_element(by=By.XPATH, value="/html/body/div/div[3]/div/div[1]/form/div[2]/div[2]/button").click()
    print('signing in...')
    
-   
+
+   # this is the solution instead of implicit or explicit wait. It only breaks the loop if the 
+   # needed element is found. Either 'Permission denied' being the text in an element, or the grades box element.
    while (True) :
-      
       try :
          if "Permission denied" in driver.find_element(by=By.XPATH, value="/html/body/div/div[3]/div/div[1]/form/div[2]/div[1]/div[2]").text :
             print('Login wrong')
@@ -37,25 +40,34 @@ def getGrades(username, password):
          break
    
 
-
+   # get the unfiltered grades with all the other garbage
    rawGrades = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div").text
    rawGrades = str(rawGrades).split("\n")
    driver.close
    
    finalGrades = []
-   
+
+   # this filters out classes, and then grades
    for posGrade in rawGrades :
       indexLength = len(posGrade)
       
       if "ABC" in posGrade :
          finalGrades.append(posGrade)
-      
+
+      # filters out grades
       if indexLength != 0 and posGrade[indexLength -1] in ['A', 'B', 'C', 'D', 'F'] and indexLength <= 6 and "," not in posGrade :
          finalGrades.append(posGrade)
          
    return finalGrades
       
 
+
+
+
+
+
+
+# BIGGG    GUI
 
 root = Tk()
 root.geometry("600x350")
